ea806930f7749cbfad67a56e2f4786fbc40fd4f3
utwoo <zhuxiang1212@hotmail.com>
2018-03-11 11:10:04 +0800

    CodeWars: Multiply

---
+++ CodeWars/8Kyu/Multiply/Kata.go
@@ -0,0 +1,6 @@
+package Multiply
+
+// Kata is just kata.
+func Kata(a, b int) int{
+	return a*b;	
+}
---
+++ CodeWars/8Kyu/Multiply/Kata_test.go
@@ -0,0 +1,9 @@
+package Multiply
+
+import "testing"
+
+func TestKata(t *testing.T){
+	if Kata(1, 1) != 1{
+		t.Error("Error")
+	}
+}
