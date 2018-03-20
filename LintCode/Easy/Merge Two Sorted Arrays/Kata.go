f5af7291fd36617c7d065623934638466cbb5d16
utwoo <zhuxiang1212@hotmail.com>
2018-03-20 18:14:42 +0800

    LintCode: Merge Two Sorted Arrays

--- LintCode/Easy/Merge Two Sorted Arrays/Kata.go
+++ LintCode/Easy/Merge Two Sorted Arrays/Kata.go
@@ -2,5 +2,28 @@ package kata
 
 // Kata http://www.lintcode.com/en/problem/merge-two-sorted-arrays/
 func mergeSortedArray(A []int, B []int) []int {
-	// write your code here
+	var (
+		result []int
+		a      int
+		b      int
+	)
+	for {
+		if A[a] < B[b] {
+			result = append(result, A[a])
+			a++
+			if a == len(A) {
+				result = append(result, B[b:]...)
+				break
+			}
+		} else {
+			result = append(result, B[b])
+			b++
+			if b == len(B) {
+				result = append(result, A[a:]...)
+				break
+			}
+		}
+	}
+
+	return result
 }
