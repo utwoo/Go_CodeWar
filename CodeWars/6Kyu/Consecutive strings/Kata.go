397cf89312a13cacc3f2d6d92ca7be2440a9af2d
utwoo <zhuxiang1212@hotmail.com>
2018-03-29 20:11:43 +0800

    CodeWars: Consecutive strings

--- CodeWars/6Kyu/Consecutive strings/Kata.go
+++ CodeWars/6Kyu/Consecutive strings/Kata.go
@@ -1,36 +1,21 @@
 package kata
 
 import (
-	"sort"
 	"strings"
 )
 
 // https://www.codewars.com/kata/consecutive-strings/go
 
-// StringSort ...
-type StringSort []string
-
-// Len ...
-func (s StringSort) Len() int {
-	return len(s)
-}
-
-// Swap ...
-func (s StringSort) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-// Less ...
-func (s StringSort) Less(i, j int) bool {
-	return len(s[i]) > len(s[j])
-}
-
 // LongestConsec ...
 func LongestConsec(strarr []string, k int) string {
-	if len(strarr) == 0 || k > len(strarr) || k <= 0 {
-		return ""
+	var buffer string
+	var largest string
+
+	for i := 0; i <= len(strarr)-k; i++ {
+		buffer = strings.Join(strarr[i : i+k][:], "")
+		if len(buffer) > len(largest) {
+			largest = buffer
+		}
 	}
-	trans := StringSort(strarr)
-	sort.Sort(trans)
-	return strings.Join(trans[:k], "")
+	return largest
 }
